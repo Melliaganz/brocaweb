90b8bc8c1b33f4177c598281071f7645a4882f47
Lucas Lengrand <lucas.lengrand@dgfip.finances.gouv.fr>
2025-07-22 11:36:30 +0200

    enregistrement login gestion de la navbar

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,63 @@
+import { useContext, useState } from "react";
+import { login } from "../../Services/api";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+import { AuthContext } from "../../Services/AuthContext";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [motDePasse, setMotDePasse] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setMessage("");
+
+    try {
+      const data = await login(email, motDePasse);
+      localStorage.setItem("token", data.token);
+      setMessage("Connexion réussie");
+      navigate("/");
+    } catch (err) {
+      setMessage(err.message);
+    } finally {
+      setIsAuthenticated(true);
+
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="loginContainer">
+      <h2>Connexion</h2>
+      <form onSubmit={handleSubmit} style={{ maxWidth: "400px" }}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          style={{ display: "block", width: "100%", marginBottom: "1rem" }}
+        />
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          value={motDePasse}
+          onChange={(e) => setMotDePasse(e.target.value)}
+          required
+          style={{ display: "block", width: "100%", marginBottom: "1rem" }}
+        />
+        <button type="submit" disabled={loading} className="btn">
+          {loading ? "Connexion..." : "Se connecter"}
+        </button>
+      </form>
+      {message && <p style={{ marginTop: "1rem", color: "red" }}>{message}</p>}
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Pages/Register/Register.js
@@ -0,0 +1,84 @@
+import { useContext, useState } from "react";
+import { register } from "../../Services/api";
+import "./register.css";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Services/AuthContext";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    nom: "",
+    email: "",
+    motDePasse: "",
+  });
+
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setMessage("");
+
+    try {
+      const data = await register(formData);
+      localStorage.setItem("token", data.token);
+      setMessage("Compte créé avec succès");
+      navigate("/");
+    } catch (err) {
+      setMessage(err.message);
+    } finally {
+      setIsAuthenticated(true);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="formulaireContainer">
+      <h2>Créer un compte</h2>
+      <form onSubmit={handleSubmit} style={{ maxWidth: "400px" }}>
+        <input
+          className="inputs"
+          type="text"
+          name="nom"
+          placeholder="Nom"
+          value={formData.nom}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Adresse email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          className="inputs"
+        />
+        <input
+          type="password"
+          name="motDePasse"
+          placeholder="Mot de passe"
+          value={formData.motDePasse}
+          onChange={handleChange}
+          required
+          className="inputs"
+        />
+        <button type="submit" disabled={loading} className="btn">
+          {loading ? "Création..." : "Créer mon compte"}
+        </button>
+      </form>
+      {message && <p style={{ marginTop: "1rem", color: "red" }}>{message}</p>}
+    </div>
+  );
+}
+
+export default Register;
